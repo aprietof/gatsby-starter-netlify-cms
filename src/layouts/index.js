eb74a037990895104bc715b6aa95ee7fb6218014
Adrian Prieto <adrianprieto00@gmail.com>
2018-07-09 22:02:01 -0400

    testing twitter card

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from '../components/Navbar';
+import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@flickr" />
+      <meta
+        name="twitter:title"
+        content="Small Island Developing States Photo Submission"
+      />
+      <meta name="twitter:description" content="View the album on Flickr." />
+      <meta
+        name="twitter:image"
+        content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg"
+      />
+    </Helmet>
     <Navbar />
     <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
